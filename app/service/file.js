aef06ebd3737b876460ba8eb2bffc4e39230f08b
ACERY1 <y841034081@gmail.com>
2019-04-17 23:31:08 +0800

    feat:完成多文件接收功能

--- app/service/file.js
+++ app/service/file.js
@@ -1,46 +1,44 @@
-const Service = require('egg').Service;
-const fs = require('mz/fs');
-const path = require('path');
-const toArray = require('stream-to-array');
-const sendToWormhole = require('stream-wormhole');
-const pump = require('mz-modules/pump');
+const Service = require("egg").Service;
+const fs = require("mz/fs");
+const fs_extra = require("fs-extra");
+const path = require("path");
+const toArray = require("stream-to-array");
+const sendToWormhole = require("stream-wormhole");
+const pump = require("mz-modules/pump");
 
 class FileRecorder extends Service {
-  async sayHello () {
-    console.log('say hello from service');
-    return 'hello'
+  async sayHello() {
+    console.log("say hello from service");
+    return "hello";
   }
 
   /**
    * 将文件拷贝到指定的目录里
    * 如果没有目录则创建目录
    */
-  async copyFiles (filePath) {
-    // const ctx = this.ctx;
-    // const parts = this.ctx.multipart({ autoFields: true })
-    // const stream = await ctx.getFileStream();
-    // let buf;
-    // try {
-    //   const parts = await toArray(stream);
-    //   buf = Buffer.concat(parts);
-    // } catch (err) {
-    //   await sendToWormhole(stream); // drop 掉
-    //   throw err;
-    // }
-    // console.log(stream)
-    // const target = path.join(this.config.baseDir, '/web/app', stream.filename);
-    // // const target = path.resolve(filePath);
-    // await fs.writeFile(target, buf);
+  async copyFiles(filePath) {
     const parts = this.ctx.multipart({ autoFields: true });
-    const files = [];
 
     let stream;
     while ((stream = await parts()) != null) {
       const filename = stream.filename.toLowerCase();
-      const target = path.join(this.config.baseDir, 'web/app', filename);
-      const writeStream = fs.createWriteStream(target);
-      await pump(stream, writeStream);
-      files.push(filename);
+      const target = path.join(
+        this.config.baseDir,
+        "web/app",
+        stream.fieldname
+      );
+      const isExists = await fs.exists(path.dirname(target));
+      if (!isExists) {
+        fs_extra.mkdirp(path.dirname(target), {}, async err => {
+          if (err) this.ctx.helper.APIError("write file failed");
+          // 异步注意：防止文件未创建好就开始写入流
+          const writeStream = fs.createWriteStream(target);
+          await pump(stream, writeStream);
+        });
+      } else {
+        const writeStream = fs.createWriteStream(target);
+        await pump(stream, writeStream);
+      }
     }
   }
 }
